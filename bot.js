7c311c643da035cb5f3e9bcb02cdcda5210a8e20
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-12-01 08:34:01 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1715,17 +1715,7 @@ client.on('message', message => {
 }
 });
 
-client.on('message', message => {
-              if (!message.channel.guild) return;
-      if(message.content =='!count')
-      var IzRo = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURL)
-      .setFooter(message.author.username, message.author.avatarURL)
-      .setTitle(':tulip:| Members info')
-      .addBlankField(true)
-      .addField('عدد اعضاء السيرفر',`${message.guild.memberCount}`)
-      message.channel.send(IzRo);
-    });
+
 
 
 
